eba8d063d8c2369d210e72de43365bcd210abc5c
Alexis THOMAS <fr.alexisthomas@gmail.com>
2015-11-01 15:17:16 +0100

    Wording in log

--- src/make.sh
+++ src/make.sh
@@ -14,7 +14,7 @@ CHROMEPACKAGE=gmanager.jar
 FILES="chrome/$CHROMEPACKAGE chrome.manifest components defaults install.rdf license.txt"
 CHROMEFILES="content locale skin"
 
-echo "Packaging using version of $VERSION"
+echo "Packaging using version $VERSION"
 echo "Package chrome jar"
 cd chrome && $ZIP $ZIPARGS $CHROMEPACKAGE $CHROMEFILES > ../../chromepackage.out
 echo "Packaging XPI $PACKAGE"
