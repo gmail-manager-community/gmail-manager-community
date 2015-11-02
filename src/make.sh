b5ed0bd3c85b1b25ef1bcd0e537cb8615670bfe1
arix98 <hoekeyz@outlook.com>
2015-11-01 18:29:09 -0800

    Change from jar to directory
    Change from chrome.jar to directory for easy developing
    
    Fix make file when Jar->directory

--- src/make.sh
+++ src/make.sh
@@ -9,17 +9,12 @@ ZIPARGS=-r
 VERSION=`sed -ne '/em:version/{s/.*<em:version>\(.*\)<\/em:version>.*/\1/p;q;}' install.rdf`
 DISTDIR=../dist
 PACKAGE=$DISTDIR/gmail_manager_community-$VERSION.xpi
-CHROMEPACKAGE=gmanager.jar
 
-FILES="chrome/$CHROMEPACKAGE chrome.manifest components defaults install.rdf license.txt"
-CHROMEFILES="content locale skin"
+FILES="chrome.manifest chrome components defaults install.rdf license.txt"
 
 echo "Cleaning"
-rm -f chrome/$CHROMEPACKAGE
 rm -f $PACKAGE
 
 echo "Packaging using version $VERSION"
-echo "Package chrome jar"
-cd chrome && $ZIP $ZIPARGS $CHROMEPACKAGE $CHROMEFILES > ../../chromepackage.out
 echo "Packaging XPI $PACKAGE"
-cd .. && $ZIP $ZIPARGS $PACKAGE $FILES > ../xpipackage.out
+$ZIP $ZIPARGS $PACKAGE $FILES > ../xpipackage.out
